0faa0f04fd519053dcc36b797942f7e5205b711f
Dinesh Ramsundar Yadav <103591369+DineshYadav2412@users.noreply.github.com>
2024-10-17 19:38:42 +0530

    Add files via upload

---
+++ one_to_one-master/src/main/java/com/sprk/one_to_one/controller/DemoController.java
@@ -0,0 +1,89 @@
+package com.sprk.one_to_one.controller;
+
+import com.sprk.one_to_one.entity.Instructor;
+import com.sprk.one_to_one.entity.InstructorDetail;
+import com.sprk.one_to_one.repository.AppDao;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class DemoController {
+
+    private final AppDao appDao;
+
+    @PostMapping("/save")
+    public Instructor saveInstructor(@RequestBody Instructor instructor) {
+
+        return appDao.saveInstructor(instructor);
+    }
+
+    @GetMapping("/getbyid/{instructorId}")
+    public Instructor getInstructorById(@PathVariable int instructorId) {
+        return appDao.getInstructorById(instructorId);
+    }
+
+    @GetMapping("/getall")
+    public List<Instructor> getAllInstructors() {
+        return appDao.getAllInstructors();
+    }
+
+    @DeleteMapping("/deletebyid/{instructorId}")
+    public String deleteByInstructorId(@PathVariable int instructorId) {
+        String message = appDao.deleteInstructorById(instructorId);
+
+        return message;
+
+    }
+
+    @PutMapping("/updatebyid/{instructorId}")
+    public Instructor updateByInstructorId(@PathVariable int instructorId, @RequestBody Instructor instructor) {
+        Instructor savedInstructor = appDao.getInstructorById(instructorId);
+        if (savedInstructor != null) {
+            InstructorDetail updatedInstructorDetail = instructor.getInstructorDetail();
+            updatedInstructorDetail.setInstructorDetailId(savedInstructor.getInstructorDetail().getInstructorDetailId());
+
+            instructor.setInstructorId(savedInstructor.getInstructorId());
+            instructor.setInstructorDetail(updatedInstructorDetail);
+
+            return appDao.saveInstructor(instructor);
+
+
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/find-detail/{instructorDetailId}")
+    public InstructorDetail getInstructorDetailById(@PathVariable int instructorDetailId) {
+
+        return appDao.findInstructorDetailById(instructorDetailId);
+    }
+
+    @PostMapping("/saveDetail")
+    public Instructor saveInstructorDetail(@RequestBody InstructorDetail instructorDetail) {
+
+        Instructor instructor = instructorDetail.getInstructor();
+        instructor.setInstructorDetail(instructorDetail);
+
+      return appDao.saveInstructor(instructor);
+
+    }
+
+    @DeleteMapping("/delete-detail/{instructorDetailId}")
+    public String deleteByInstructorDetailId(@PathVariable int instructorDetailId) {
+        String message = appDao.deleteInstructorDetailById(instructorDetailId);
+
+        return message;
+
+    }
+
+    @PutMapping("/update-detail/{instructorId}")
+    public Instructor updateInstructorDetail(@PathVariable int instructorId, @RequestBody InstructorDetail instructorDetail) {
+
+        return appDao.updateInstrutorDetail(instructorId, instructorDetail);
+    }
+}
+
---
+++ one_to_one-master/src/main/java/com/sprk/one_to_one/entity/Instructor.java
@@ -0,0 +1,26 @@
+package com.sprk.one_to_one.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int instructorId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor_detail_id")
+    @JsonManagedReference
+    private InstructorDetail instructorDetail;
+
+}
